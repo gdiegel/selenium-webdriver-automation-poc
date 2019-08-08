71a7cf71ec4d696f231334cfdbc3e9e872021f9b
Gabriel Diegel <gabriel.diegel@1und1.de>
2019-08-08 14:45:46 +0200

    Add assertJ and assertion to page object

--- src/main/java/com/unitedinternet/mam/qa/specification/SearchPage.java
+++ src/main/java/com/unitedinternet/mam/qa/specification/SearchPage.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SearchPage extends Page {
 
     public SearchPage(WebDriver driver) {
         super(driver);
+        assertThat(searchBar.isDisplayed()).isTrue();
     }
 
     @FindBy(css = ".pos-search-field__input")
